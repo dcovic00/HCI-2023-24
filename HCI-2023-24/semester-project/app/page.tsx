3625e095f6f77fd23eb0e9854695fc29916ed6f4
dcovic00 <147554469+dcovic00@users.noreply.github.com>
2023-10-24 11:45:32 +0200

    Add files via upload

---
+++ HCI-2023-24/semester-project/app/page.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+ 
+
+export default function Home(){
+  return (
+    <main>
+      <h1 className="mainpage"> Welcome Message </h1>
+
+      <p className="mainpage"> About Us </p>
+
+      <p className="mainpage"> Contact Us </p>
+    </main>
+  );
+}
+
